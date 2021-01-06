ea9546450bd09964e7ce2f62c682409bebec88f0
misztijanos <misztijanos@yahoo.com>
2021-01-06 12:05:54 +0200

    added db connection

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // connect to mongodb & listen for requests
 const dbURI =
-  "mongodb+srv://netninja:test1234@net-ninja-tuts-del96.mongodb.net/node-tuts";
+  "mongodb+srv://janos:test1234@cluster0.gv7yn.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
